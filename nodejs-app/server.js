de4f723126532a4f243ea06cc4a2d0f826844562
Student User <student@workstation.lab.example.com>
2022-09-01 21:51:33 -0400

    toto

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,8 +4,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
-  res.send('Hello World from Toto: ' + ${date} + '\n')
+  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n' + Hello World from Toto: ' + ${date} + '\n')
 });
 
 app.listen(8080, function () {
